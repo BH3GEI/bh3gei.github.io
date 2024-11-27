185107ba8b8887de7f1eac3de00367808915f4c8
BH3GEI <ufp_enterprise@yahoo.com>
2024-11-27 21:52:52 +0900

    Update configuration and deployment settings

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/bh3gei.github.io/' : '/',
-  outputDir: 'docs',
-  productionSourceMap: false
+  publicPath: '',
+  outputDir: 'dist',
+  productionSourceMap: false,
+  // 确保不会覆盖重要文件
+  indexPath: 'index.html'
 })
