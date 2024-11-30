468ee3eaa867e4fb0951429dde29d87f2a166197
BH3GEI <58540850+BH3GEI@users.noreply.github.com>
2024-12-01 01:45:38 +0900

    Switch to docs folder deployment

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: '',
-  outputDir: 'dist',
+  publicPath: '/',
+  outputDir: 'docs',
   productionSourceMap: false,
   // 确保不会覆盖重要文件
   indexPath: 'index.html'
